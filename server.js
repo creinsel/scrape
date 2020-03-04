1c17a7caed8190d2a76599e13db97872f6b7039c
Cassondra Reinsel <cassiereinsel@gmail.com>
2020-03-04 08:45:22 -0800

    server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var logger = require("morgan");
+var exphbs = require("express-handlebars");
 var mongoose = require("mongoose");
 
 // Our scraping tools
@@ -11,23 +11,23 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
 
-// Configure middleware
 
-// Use morgan logger for logging requests
-app.use(logger("dev"));
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/scrape", { useNewUrlParser: true });
 
 // Routes
 
