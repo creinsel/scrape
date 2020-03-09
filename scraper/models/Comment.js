6aa389d3da4022b8cebc85bf614ecc8810cc651f
Cassondra Reinsel <cassiereinsel@gmail.com>
2020-03-09 15:11:30 -0700

    models, handlebars

---
+++ scraper/models/Comment.js
@@ -0,0 +1,16 @@
+var Schema = mongoose.Schema;
+var CommentSchema = new Schema({
+    name: {
+        type: String,
+        
+    },
+    body: {
+        type: String,
+        required: true
+    }
+    
+})
+
+var Comment = mongoose.model("Comment", CommentSchema);
+
+module.exports = Comment;
